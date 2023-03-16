40eb9d2b95ecb0a226fdc46ac4ff9a926500da7c
Laurent Reynders <laurent_reynders@hotmail.com>
2023-03-16 21:11:28 +0100

    test unicode 1

---
+++ config/unicode.h
@@ -0,0 +1,14 @@
+// #pragma once
+
+// #include "../zmk-nodefree-config/helper.h"
+// #include "../zmk-nodefree-config/keypos_def/keypos_58keys.h"
+
+// ZMK_BEHAVIOR(circonflex, macro,
+//      wait-ms = <10>;
+//      tap-ms = <5>;
+//      bindings
+//      = <&macro_press &kp LALT>
+//      , <&macro_tap &kp N0 &kp N0 &kp N5 &kp E>
+//      , <&macro_release &kp LALT>
+//      ;
+// )
